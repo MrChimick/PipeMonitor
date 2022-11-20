e5e29d83bb333d8b3c36ef990dc1af5527525c2c
MrChimick <mikechimick@gmail.com>
2022-11-19 16:26:35 -0800

    Update Temp_Logger.ino
    Updated temperature reading and flow compare for the new 5 sensor structure

--- Temp_Logger/Temp_Logger.ino
+++ Temp_Logger/Temp_Logger.ino
@@ -2,8 +2,10 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
+#include <SoftwareSerial.h>
 
 #define DS1307_ADDRESS 0x68
+
 
 /************************************************
    DATA STRUCTURES
@@ -33,84 +35,76 @@ const byte ZERO = 0x00;
 
 const byte TEMP_AVG_ARRAY = 5;
 
-const long TEMP_READ_INTERVAL = 1000;
+const long TEMP_READ_INTERVAL    = 1000;
 const long TEMP_COMPARE_INTERVAL = 30000; //CHANGE TO 900000 in for final
 
-const byte NUM_SEC = 60;
-const byte NUM_MIN = 60;
+const int TEMP_ROOF_FLOW_THRESHOLD  = 5;
+const int TEMP_HOUSE_FLOW_THRESHOLD = 5;
+
+const byte NUM_SEC  = 60;
+const byte NUM_MIN  = 60;
 const byte NUM_HOUR = 24;
 
 const int STARTUP_DELAY = 1000;
 const int STARTUP_SPEED = 9600;
 
+const float RES_OHM = 10000; // Resistance of the resistor, in ohms
+const float SH_C1 = 1.009249522e-03, SH_C2 = 2.378405444e-04, SH_C3 = 2.019202697e-07; // Steinhart–Hart coefficients
+
 // Pin constants
-const byte BT_PIN_THERMO_DO = 39;
-const byte BT_PIN_THERMO_CS = 40;
-const byte BT_PIN_THERMO_CLK = 41;
+const byte PIN_TANK_HIGHER = 10;
+const byte PIN_TANK_LOWER  = 10;
+const byte PIN_ROOF_LOOP   = 10;
+const byte PIN_HOUSE_LOOP  = 10;
 
-const byte HR_PIN_THERMO_DO = 42;
-const byte HR_PIN_THERMO_CS = 43;
-const byte HR_PIN_THERMO_CLK = 44;
+const byte PIN_MANIFOLD_RX = 10;
+const byte PIN_MANIFOLD_TX = 10;
 
-const byte CP_PIN_THERMO_DO = 62;
-const byte CP_PIN_THERMO_CS = 63;
-const byte CP_PIN_THERMO_CLK = 64;
+const byte PIN_SD_CS = 15;
 
-const byte SD_PIN_CS = 15;
-
-const byte CP_PIN_FLOW = 69;
-
-const byte RELAY_PIN = 65;
-const byte CIRC_PIN = 66;
+const byte PIN_FLOW_ROOF_LOOP  = 66;
+const byte PIN_FLOW_HOUSE_LOOP = 65;
 
 // String constants
 const String FILE_TEMPERATURE = "temp_log.csv";
-const String FILE_FLOW_TIME = "flow_log.csv";
+const String FILE_FLOW_TIME   = "flow_log.csv";
 const String HEAD_TEMPERATURE = "Time,Location,Temperature";
-const String HEAD_FLOW_TIME = "Time Started,Time Ended,Duration,Location";
+const String HEAD_FLOW_TIME   = "Time Started,Time Ended,Duration,Location";
 
-const String BT_LOCATION_STR = "Boiler Tank";
-const String HR_LOCATION_STR = "Heat Return Pipe";
-const String CP_LOCATION_STR = "Collector Pipe";
-
-String inputstring = "";                              //a string to hold incoming data from the PC
-String sensorstring = "";                             //a string to hold the data from the Atlas Scientific product
-boolean input_string_complete = false;                //have we received all the data from the PC
-boolean sensor_string_complete = false;               //have we received all the data from the Atlas Scientific product
-float temperature;                                    //used to hold a floating point number that is the RTD temperature
-
-
+const String LOC_STR_TANK       = "Tank";
+const String LOC_STR_ROOF_LOOP  = "Roof Loop";
+const String LOC_STR_HOUSE_LOOP = "House Loop";
+const String LOC_STR_MANIFOLD   = "Manifold";
 
 
 /************************************************
    GLOBAL VARIABLES
  ***********************************************/
-// Temperature sensor objects
-MAX6675 TankTempSensor(BT_PIN_THERMO_CLK, BT_PIN_THERMO_CS, BT_PIN_THERMO_DO);
-MAX6675 HeatReturnTempSensor(HR_PIN_THERMO_CLK, HR_PIN_THERMO_CS, HR_PIN_THERMO_DO);
-MAX6675 CollectorTempSensor(CP_PIN_THERMO_CLK, CP_PIN_THERMO_CS, CP_PIN_THERMO_DO);
-
 // Temperature collection variables
 byte arrayIndex = 0;
 
 float tankTempArray[TEMP_AVG_ARRAY];
 float tankTempAvg = 0.0;
 
-float heatReturnTempArray[TEMP_AVG_ARRAY];
-float heatReturnTempAvg = 0.0;
+float roofLoopTempArray[TEMP_AVG_ARRAY];
+float roofLoopTempAvg = 0.0;
 
-float collectorLoopArray[TEMP_AVG_ARRAY];
-float collectorLoopAvg = 0.0;
+float houseLoopTempArray[TEMP_AVG_ARRAY];
+float houseLoopTempAvg = 0.0;
 
-float tempDiff = 10;
+float manifoldTempArray[TEMP_AVG_ARRAY];
+float manifoldTempAvg = 0.0;
 
 // Time interval counters
 unsigned long previousMillisRead = 0;
 unsigned long previousMillisCompare = 0;
 
 // Flow variables
-FlowTracker heatReturnFlowTracker;
-FlowTracker collectorFlowTracker;
+FlowTracker roofLoopFlowTracker;
+FlowTracker houseLoopFlowTracker;
+
+// RTD Serial Connection
+SoftwareSerial manifoldSerial(PIN_MANIFOLD_RX, PIN_MANIFOLD_TX);
 
 
 /************************************************
@@ -132,28 +126,58 @@ void Serial2LineEnd() {
 
 String DateTimeToString(DateTime timePrint) { // return the date EG 21/10/5 23:59:59
   return (
-           String(timePrint.year) + '/' +
-           String(timePrint.month) + '/' +
-           String(timePrint.monthDay) + ' ' +
-           String(timePrint.hour) + ':' +
-           String(timePrint.minute) + ':' +
-           String(timePrint.second)
-         );
+    String(timePrint.year)     + '/' +
+    String(timePrint.month)    + '/' +
+    String(timePrint.monthDay) + ' ' +
+    String(timePrint.hour)     + ':' +
+    String(timePrint.minute)   + ':' +
+    String(timePrint.second)
+  );
 }
 
+float ReadThermistor(byte pin) { // returns the reading from a thermistor in Celcius
+  int v = analogRead(pin);
+  float r = RES_OHM * (1023.0 / (float)v - 1.0); // resistance of the Thermistor
+  float logR = log(r);
+  float tKelvin = (1.0 / (SH_C1 + SH_C2 * logR + SH_C3 * logR * logR * logR));
+  float tCelsius = tKelvin - 273.15;
+
+  return tCelsius;
+}
+
+float ReadRTD(SoftwareSerial rtdSerial) {
+  // command for a single reading, default units are Celcius
+  rtdSerial.print("R\r"); 
+
+  // recieves the response from the sensor
+  String sensorstring = "";
+  while (rtdSerial.available() > 0) {
+    char inchar = (char)rtdSerial.read();
+    sensorstring += inchar;
+    if (inchar == '\r') break;
+  }
+  
+  // converts response to a number
+  float temp = 0.0;
+  if (isdigit(sensorstring[0]) || sensorstring[0] == '-') {
+    temp = sensorstring.toFloat();
+  }
+  
+  return temp;
+}
 
 /************************************************
    DATE & TIME FUNCTIONS
  ***********************************************/
 void SetDateTime() {
 
-  byte second = 45; //0-59
-  byte minute = 40; //0-59
-  byte hour = 0; //0-23
-  byte weekDay = 2; //1-7
+  byte second   = 45; //0-59
+  byte minute   = 40; //0-59
+  byte hour     = 0; //0-23
+  byte weekDay  = 2; //1-7
   byte monthDay = 1; //1-31
-  byte month = 3; //1-12
-  byte year = 11; //0-99
+  byte month    = 3; //1-12
+  byte year     = 11; //0-99
 
   Wire.beginTransmission(DS1307_ADDRESS);
   Wire.write(ZERO);
@@ -180,13 +204,13 @@ DateTime GetCurrentDateTime() {
   Wire.requestFrom(DS1307_ADDRESS, 7);
 
   DateTime currDateTime;
-  currDateTime.second = BcdToDec(Wire.read());
-  currDateTime.minute = BcdToDec(Wire.read());
-  currDateTime.hour = BcdToDec(Wire.read() & 0b111111); //24 hour time
-  currDateTime.weekDay = BcdToDec(Wire.read()); //0-6 -> sunday â€“ Saturday
+  currDateTime.second   = BcdToDec(Wire.read());
+  currDateTime.minute   = BcdToDec(Wire.read());
+  currDateTime.hour     = BcdToDec(Wire.read() & 0b111111); //24 hour time
+  currDateTime.weekDay  = BcdToDec(Wire.read()); //0-6 -> sunday â€“ Saturday
   currDateTime.monthDay = BcdToDec(Wire.read());
-  currDateTime.month = BcdToDec(Wire.read());
-  currDateTime.year = BcdToDec(Wire.read());
+  currDateTime.month    = BcdToDec(Wire.read());
+  currDateTime.year     = BcdToDec(Wire.read());
 
   return currDateTime;
 }
@@ -218,94 +242,29 @@ int GetMinutesBetweenDateTime(DateTime startTime, DateTime endTime) { // Assumes
   return round(seconds / NUM_SEC);
 }
 
+void PrintCurrentDateTime() {
+  DateTime currTime; // declaring a variable of type DateTime
+  currTime = GetCurrentDateTime(); // getting the current DateTime using the function
+
+  // accessing all members
+  Serial.println("Second: "   + String(currTime.second));
+  Serial.println("Minute: "   + String(currTime.minute));
+  Serial.println("Hour: "     + String(currTime.hour));
+  Serial.println("WeekDay: "  + String(currTime.weekDay));
+  Serial.println("MonthDay: " + String(currTime.monthDay));
+  Serial.println("Month: "    + String(currTime.month));
+  Serial.println("Year: "     + String(currTime.year));
+
+  // I can change the members as well
+  currTime.second = 0;
+}
+
 
 /************************************************
-   MAIN FUNCTIONS
+   SD FUNCTIONS
  ***********************************************/
-void TempRead() { //Reads temps every second, creates average, and sends values to Nextion
-  tankTempArray[arrayIndex] = (TankTempSensor.readCelsius());
-  heatReturnTempArray[arrayIndex] = (HeatReturnTempSensor.readCelsius());
-  collectorLoopArray[arrayIndex] = (CollectorTempSensor.readCelsius());
-  tankTempAvg = 0.0;
-  heatReturnTempAvg = 0.0;
-  collectorLoopAvg = 0.0;
-
-  for (byte i = 0; i < TEMP_AVG_ARRAY; i++) {
-    tankTempAvg += tankTempArray[i];
-    heatReturnTempAvg += heatReturnTempArray[i];
-    collectorLoopAvg += collectorLoopArray[i];
-  }
-
-  tankTempAvg /= TEMP_AVG_ARRAY;
-  heatReturnTempAvg /= TEMP_AVG_ARRAY;
-  collectorLoopAvg /= TEMP_AVG_ARRAY;
-
-  arrayIndex++;
-  if (arrayIndex >= TEMP_AVG_ARRAY) arrayIndex = 0;
-
-  Serial2.print("Status.t8.txt=\"" + String(round(tankTempAvg)) + "\xB0" + "C\"");
-  Serial2LineEnd();
-
-  Serial2.print("t9.txt=\"" + String(round(heatReturnTempAvg)) + "\xB0" + "C\"");
-  Serial2LineEnd();
-
-  Serial2.print("t10.txt=\"" + String(round(collectorLoopAvg)) + "\xB0" + "C\"");
-  Serial2LineEnd();
-
-  Serial.println(collectorLoopAvg);
-
-  Serial.println(tankTempAvg);
-
-}
-
-void TempCompare() { //Compares temps every 15mins, updates valve relay, writes, time, temps
-  if (tankTempAvg > heatReturnTempAvg) {
-    if (heatReturnFlowTracker.flowStatus != true) { // flow just turned on
-      digitalWrite(RELAY_PIN, LOW); // set pin 10 LOW
-      heatReturnFlowTracker.flowStatus = true;
-      heatReturnFlowTracker.startTime = GetCurrentDateTime();
-      Serial2.print("t11.bco=2016");
-      Serial2LineEnd();
-    }
-  } else if (heatReturnFlowTracker.flowStatus == true) { // flow just turned off
-    digitalWrite(RELAY_PIN, HIGH); // set pin 10 HIGH
-    heatReturnFlowTracker.endTime = GetCurrentDateTime();
-    SD_LogFlow(HR_LOCATION_STR, heatReturnFlowTracker);
-    heatReturnFlowTracker.flowStatus = false;
-    Serial2.print("t11.bco=63488");
-    Serial2LineEnd();
-  }
-}
-
-void LogAllTemperatures() {
-  SD_LogTemp(BT_LOCATION_STR, tankTempAvg);
-  SD_LogTemp(HR_LOCATION_STR, heatReturnTempAvg);
-  SD_LogTemp(CP_LOCATION_STR, collectorLoopAvg);
-}
-
-void FlowDetection() {
-  if (digitalRead(CP_PIN_FLOW) == LOW) {
-    if (collectorFlowTracker.flowStatus != true) { // flow just turned on
-      collectorFlowTracker.flowStatus = true;
-      collectorFlowTracker.startTime = GetCurrentDateTime();
-      Serial2.print("t12.bco=2016");
-      Serial2LineEnd();
-    }
-  } else if (collectorFlowTracker.flowStatus == true) { // flow just turned off
-    collectorFlowTracker.endTime = GetCurrentDateTime();
-    SD_LogFlow(CP_LOCATION_STR, collectorFlowTracker);
-    collectorFlowTracker.flowStatus = false;
-    Serial2.print("t12.bco=63488");
-    Serial2LineEnd();
-  }
-}
-
-void Midnight() {
-  //TODO: Update Nextion time
-}
-
 void SD_Init() {
-  if (!SD.begin(SD_PIN_CS)) {
+  if (!SD.begin(PIN_SD_CS)) {
     Serial.println("SD initialization failed");
     return;
   }
@@ -347,50 +306,100 @@ void SD_LogFlow(String location, FlowTracker flowEntry) {
   }
 }
 
-void PrintCurrentDateTime() {
-  DateTime currTime; // declaring a variable of type DateTime
-  currTime = GetCurrentDateTime(); // getting the current DateTime using the function
-
-  // accessing all members
-  Serial.println("Second: " + String(currTime.second));
-  Serial.println("Minute: " + String(currTime.minute));
-  Serial.println("Hour: " + String(currTime.hour));
-  Serial.println("WeekDay: " + String(currTime.weekDay));
-  Serial.println("MonthDay: " + String(currTime.monthDay));
-  Serial.println("Month: " + String(currTime.month));
-  Serial.println("Year: " + String(currTime.year));
-
-  // I can change the members as well
-  currTime.second = 0;
+void LogAllTemperatures() {
+  SD_LogTemp(LOC_STR_HOUSE_LOOP, houseLoopTempAvg);
+  SD_LogTemp(LOC_STR_TANK, tankTempAvg);
+  SD_LogTemp(LOC_STR_ROOF_LOOP, roofLoopTempAvg);
+  SD_LogTemp(LOC_STR_MANIFOLD, manifoldTempAvg);
 }
 
-void serialEvent3() {                                 //if the hardware serial port_3 receives a char
-  sensorstring = Serial3.readStringUntil(13);         //read the string until we see a <CR>
-  sensor_string_complete = true;                      //set the flag used to tell if we have received a completed string from the PC
-}
+/************************************************
+   MAIN FUNCTIONS
+ ***********************************************/
+void TempRead() { //Reads temps every second, creates average, and sends values to Nextion
+  // read in temperatures with the helper functions
+  tankTempArray[arrayIndex]      = ((ReadThermistor(PIN_TANK_HIGHER) + ReadThermistor(PIN_TANK_LOWER)) / 2); // average two tank sensors
+  roofLoopTempArray[arrayIndex]  = ReadThermistor(PIN_ROOF_LOOP);
+  houseLoopTempArray[arrayIndex] = ReadThermistor(PIN_HOUSE_LOOP);
+  manifoldTempArray[arrayIndex]  = ReadRTD(manifoldSerial);
+  
+  // reset averages
+  tankTempAvg      = 0.0;
+  roofLoopTempAvg  = 0.0;
+  houseLoopTempAvg = 0.0;
+  manifoldTempAvg  = 0.0;
 
-void collectorTemp() {
-  Serial3.print('R');
-  serialEvent3();
-  if (sensor_string_complete == true) {                          //if a string from the Atlas Scientific product has been received in its entirety
-    Serial.println(sensorstring);                                //send that string to the PC's serial monitor
-    //uncomment this section to see how to convert the reading from a string to a float
-    if (isdigit(sensorstring[0]) || sensorstring[0] == '-') {    //if the first character in the string is a digit or a "-" sign
-      temperature = sensorstring.toFloat();                      //convert the string to a floating point number so it can be evaluated by the Arduino
-      if ((temperature + (tempDiff)) >= (tankTempAvg)) {                               //if the RTD temperature is greater than or equal to 25 C
-        digitalWrite(CIRC_PIN, LOW);                                  //print "high" this is demonstrating that the Arduino is evaluating the RTD temperature as a number and not as a string
-      }
-      else  {                              //if the RTD temperature is less than or equal to 24.999 C
-        digitalWrite(CIRC_PIN, HIGH);                                   //print "low" this is demonstrating that the Arduino is evaluating the RTD temperature as a number and not as a string
-      }
-    }
-
-    sensorstring = "";                                             //clear the string:
-    sensor_string_complete = false;                                //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
+  // sum for averages
+  for (byte i = 0; i < TEMP_AVG_ARRAY; i++) {
+    tankTempAvg      += tankTempArray[i];
+    roofLoopTempAvg  += roofLoopTempArray[i];
+    houseLoopTempAvg += houseLoopTempArray[i];
+    manifoldTempAvg  += manifoldTempArray[i];
   }
-  Serial2.print("Status.t10.txt=\"" + String(round(temperature)) + "\xB0" + "C\"");
+
+  // divide for averages
+  tankTempAvg      /= TEMP_AVG_ARRAY;
+  roofLoopTempAvg  /= TEMP_AVG_ARRAY;
+  houseLoopTempAvg /= TEMP_AVG_ARRAY;
+  manifoldTempAvg  /= TEMP_AVG_ARRAY;
+
+  // ensure temp arrays only store certain number before overwriting old values
+  arrayIndex++;
+  if (arrayIndex >= TEMP_AVG_ARRAY) arrayIndex = 0;
+
+  // send values to nextion
+  Serial2.print("Status.t8.txt=\"" + String(round(tankTempAvg)) + "\xB0" + "C\"");
+  Serial2LineEnd();
+  Serial2.print("t9.txt=\"" + String(round(houseLoopTempAvg)) + "\xB0" + "C\"");
+  Serial2LineEnd();
+  Serial2.print("t10.txt=\"" + String(round(roofLoopTempAvg)) + "\xB0" + "C\"");
   Serial2LineEnd();
 }
+
+void TempCompare() { // Compares temps every 15mins, updates valve relay, writes, time, temps
+  float houseLoopDiff = tankTempAvg - houseLoopTempAvg; // only flows if tank is hotter than house
+  float roofLoopDiff = roofLoopTempAvg - tankTempAvg; // only flows if roof is hotter than tank
+
+  if (houseLoopDiff > TEMP_HOUSE_FLOW_THRESHOLD) { // flow should be on
+    if (houseLoopFlowTracker.flowStatus != true) { // flow needs to turn on
+      digitalWrite(PIN_FLOW_HOUSE_LOOP, LOW); // opens flow
+      houseLoopFlowTracker.flowStatus = true;
+      houseLoopFlowTracker.startTime = GetCurrentDateTime();
+      Serial2.print("t11.bco=2016");
+      Serial2LineEnd();
+    }
+  } else if (houseLoopFlowTracker.flowStatus == true) { // flow needs to turn off
+    digitalWrite(PIN_FLOW_HOUSE_LOOP, HIGH); // closes flow
+    houseLoopFlowTracker.endTime = GetCurrentDateTime();
+    SD_LogFlow(LOC_STR_HOUSE_LOOP, houseLoopFlowTracker);
+    houseLoopFlowTracker.flowStatus = false;
+    Serial2.print("t11.bco=63488");
+    Serial2LineEnd();
+  }
+
+  if (roofLoopDiff > TEMP_ROOF_FLOW_THRESHOLD) { // flow should be on
+    if (roofLoopFlowTracker.flowStatus != true) { // flow needs to turn on
+      digitalWrite(PIN_FLOW_ROOF_LOOP, LOW); // opens flow
+      roofLoopFlowTracker.flowStatus = true;
+      roofLoopFlowTracker.startTime = GetCurrentDateTime();
+      //Serial2.print("t11.bco=2016"); // TODO: Replace with correct nextion label
+      //Serial2LineEnd();
+    }
+  } else if (roofLoopFlowTracker.flowStatus == true) { // flow needs to turn off
+    digitalWrite(PIN_FLOW_ROOF_LOOP, HIGH); // closes flow
+    roofLoopFlowTracker.endTime = GetCurrentDateTime();
+    SD_LogFlow(LOC_STR_ROOF_LOOP, roofLoopFlowTracker);
+    roofLoopFlowTracker.flowStatus = false;
+    //Serial2.print("t11.bco=63488"); // TODO: Replace with correct nextion label
+    //Serial2LineEnd();
+  }
+}
+
+void Midnight() {
+  //TODO: Update Nextion time
+}
+
+
 /************************************************
    SYSTEM FUNCTIONS
  ***********************************************/
@@ -399,16 +408,15 @@ void setup() {
   Wire.begin();
   Serial2.begin(STARTUP_SPEED);
   SetDateTime();
-  Serial3.begin(STARTUP_SPEED);
-  inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
-  sensorstring.reserve(30);                           //set aside some bytes for receiving data from Atlas Scientific product
-  pinMode(RELAY_PIN, OUTPUT); // Pump Relay pin set as output
-   pinMode(CIRC_PIN, OUTPUT);
-  pinMode(SD_PIN_CS, OUTPUT); // SPI bus
-  pinMode(CP_PIN_FLOW, INPUT_PULLUP); // Flow detection
+  
+  pinMode(PIN_FLOW_ROOF_LOOP, OUTPUT);
+  pinMode(PIN_FLOW_HOUSE_LOOP, OUTPUT);
+  pinMode(PIN_SD_CS, OUTPUT);
+  
   Serial.println("Initializing");
   // wait for MAX chip to stabilize
   delay(STARTUP_DELAY);
+  
   SD_Init();
 }
 
@@ -418,8 +426,6 @@ void loop() {
   if ((currentMillis - previousMillisRead) >= TEMP_READ_INTERVAL) {
     previousMillisRead = currentMillis;
     TempRead();
-    FlowDetection();
-    collectorTemp();
   }
 
   if ((currentMillis - previousMillisCompare) >= TEMP_COMPARE_INTERVAL) {
